412c328899c67d09b824b834ecf8ac7b29ca62e2
Eduardo Pires da Silva <101749917+EOEDUZADA@users.noreply.github.com>
2022-11-12 04:49:40 -0300

    Update script.js

--- TestePWA/js/script.js
+++ TestePWA/js/script.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
-        '/DBO-2022/TestePWA/',
+        '/TestePWA/',
         '/index.html',
         '/styles.css',
         '/script.js',
