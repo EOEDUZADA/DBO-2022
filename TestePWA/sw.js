1b2b2de89d50b0b459224c1998c3c00d7637bd58
Eduardo Pires da Silva <101749917+EOEDUZADA@users.noreply.github.com>
2022-11-15 21:56:39 -0300

    Update sw.js

--- TestePWA/sw.js
+++ TestePWA/sw.js
@@ -1,41 +1,32 @@
-var CACHE_NAME = 'static-v1';
+//consts utilizadas
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './',
+  './manifest.json'
+];
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/manifest.json',
-      ]);
-    })
-  )
-});
+//ao instalar
+self.addEventListener('install', event =>
+  event.waitUntil(cacheResources())
+)
 
-self.addEventListener('activate', function activator(event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys
-        .filter(function (key) {
-          return key.indexOf(CACHE_NAME) !== 0;
-        })
-        .map(function (key) {
-          return caches.delete(key);
-        })
-      );
-    })
-  );
-});
+async function cacheResources() {
+  const cache = await caches.open(CACHE_NAME)
+  return cache.addAll(urlsToCache)
+}
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
+//ao fazer uma requisição (fetch
 
+self.addEventListener('fetch', event =>
+  event.respondWith(cachedResource(event.request))
+)
 
-
+async function cachedResource (req) {
+  const cache = await caches.open(CACHE_NAME)
+  const response = await cache.match(req)
+  if (response) {
+    return response;
+  }
+  return fetch(req);
+}
